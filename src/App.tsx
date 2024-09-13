9d7f2af1b9e88068b637b584d4d5c799ac568899
NinaVisochina <114816977+NinaVisochina@users.noreply.github.com>
2024-09-13 23:16:09 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CategoryCreatePage from './components/create'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <CategoryCreatePage />
+    </>
+  )
+}
+
+export default App
+
